03d5acf747d1884b9d064e2d9c5214a5f851aa61
ACLC <ACLC@SLPC23>
2023-11-15 02:21:23 -0800

    Add project files.

---
+++ WebDev2/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebDev2.DataConnection;
+
+namespace WebDev2.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly MySqlDbContext _context;
+
+        public ProductController(MySqlDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task <IActionResult> Index()
+        {
+            return View(await _context.Products.ToListAsync());
+        }
+    }
+}
